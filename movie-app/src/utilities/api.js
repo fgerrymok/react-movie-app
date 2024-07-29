3e88b51a31d8996f01610feaf0a4b470e8d2f2b7
Frazer <fgerrymok@gmail.com>
2024-07-29 14:36:19 -0700

    Moved the fetch request to api.js

--- movie-app/src/utilities/api.js
+++ movie-app/src/utilities/api.js
@@ -1,3 +1,13 @@
 const API = import.meta.env.VITE_MOVIE_API_KEY;
 
-export default API;
+async function callAPI() {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${API}`
+  );
+
+  const data = await response.json();
+  const movieData = data.results;
+  return movieData;
+}
+
+export default callAPI;
