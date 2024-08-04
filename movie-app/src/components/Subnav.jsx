36c6dfd5a973d79348d7173ed20e9b920c684acb
Frazer <fgerrymok@gmail.com>
2024-08-04 13:45:17 -0700

    Transferred fetch request into Subnav component. Updated API endpoint to dynamically accept different movie categories.

--- movie-app/src/components/Subnav.jsx
+++ movie-app/src/components/Subnav.jsx
@@ -1,35 +1,57 @@
-import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import callAPI from "../utilities/api";
+const API = import.meta.env.VITE_MOVIE_API_KEY;
 
 function Subnav() {
   const [movies, setMovies] = useState([]);
+  const [category, setCategory] = useState("popular");
   const baseImgUrl = "http://image.tmdb.org/t/p/w185";
 
   useEffect(() => {
     async function generateMovies() {
-      const movieData = await callAPI();
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1&api_key=${API}`
+      );
+      const data = await response.json();
+      const movieData = data.results;
       setMovies(movieData);
     }
     generateMovies();
-  }, []);
+  }, [category]);
+
+  function updateCategory(category) {
+    setCategory(category);
+  }
 
   return (
     <>
-      <ul className="sub-nav">
-        <li>
-          <Link to="#">Popular</Link>
-        </li>
-        <li>
-          <Link to="#">Top Rated</Link>
-        </li>
-        <li>
-          <Link to="#">Upcoming</Link>
-        </li>
-        <li>
-          <Link to="#">Now Playing</Link>
-        </li>
-      </ul>
+      <button
+        onClick={() => {
+          updateCategory("popular");
+        }}
+      >
+        Popular
+      </button>
+      <button
+        onClick={() => {
+          updateCategory("top_rated");
+        }}
+      >
+        Top Rated
+      </button>
+      <button
+        onClick={() => {
+          updateCategory("upcoming");
+        }}
+      >
+        Upcoming
+      </button>
+      <button
+        onClick={() => {
+          updateCategory("now_playing");
+        }}
+      >
+        Now Playing
+      </button>
       <div className="movies-container">
         {movies &&
           movies.map((movie) => {
