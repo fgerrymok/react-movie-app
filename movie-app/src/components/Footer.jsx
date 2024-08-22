103cdcea33fd7ca6553894578a4d8867bfee858d
keanna <keannamalien@gmail.com>
2024-08-22 15:42:29 -0700

    styled subnav buttons and hover overlay for movie cards, styled footer

--- movie-app/src/components/Footer.jsx
+++ movie-app/src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import "../styles/Footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer>
-      <p className="footer-logo">Movie App</p>
-      <p className="credits">Credits: Keanna, Fatima, & Frazer</p>
+      <div>
+        <Link to="/"><img src="/clickflicks.png" alt="click flicks logo" className="footer-logo" /></Link>
+      </div>
     </footer>
   );
 }
