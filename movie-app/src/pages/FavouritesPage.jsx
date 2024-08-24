bf646c3276c5f38b5546316069ea049ef9fe88df
Frazer <fgerrymok@gmail.com>
2024-08-23 17:08:44 -0700

    Fixed favourites

--- movie-app/src/pages/FavouritesPage.jsx
+++ movie-app/src/pages/FavouritesPage.jsx
@@ -19,6 +19,16 @@ export default function Favourites() {
     </g>
     </svg>)
 
+  const addToFavouritesSvg = (
+    <svg className="more-info" viewBox="0 0 800 800" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <circle cx="400" cy="400" r="400" fill="#D3D3DD" opacity="0.5"></circle>
+      <path d="M200,400h200M400,400h200M400,400v200M400,400V200" stroke="#FFFFFF" stroke-width="66.6667" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="133.3333" fill="none"></path>
+    </g>
+    </svg>)
+
   const moreInformationSvg = (
     <svg className="more-info" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle cx="12" cy="12" r="10" stroke="#FC4A78" stroke-width="1.5"></circle> <path d="M12 17V11" stroke="#FC4A78" stroke-width="1.5" stroke-linecap="round"></path> <circle cx="1" cy="1" r="1" transform="matrix(1 0 0 -1 11 9)" fill="#FC4A78"></circle> </g></svg>)
 
@@ -31,8 +41,8 @@ export default function Favourites() {
   }
 
   return (
-    <div>
-      <h2 className={Object.keys(favouritedMovies).length === 0 ? "show-message" : "hide-message"}>You have no favourites.</h2>;
+    <div className="main-body">
+      <h2 className={Object.keys(favouritedMovies).length === 0 ? "show-message" : "hide-message"}>You have no movies on your Favorites. Add your favorite movies later by clicking . Add to Favorites.</h2>;
       <div className="favourites-movie-container">
         {Object.keys(favouritedMovies).map((key) => {
           const stringifiedObject = localStorage.getItem(key);
