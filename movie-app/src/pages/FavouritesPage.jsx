a02c161174a67775f631a86fddc6c180ad02e4bb
keanna <keannamalien@gmail.com>
2024-08-24 19:59:25 -0700

    removed semi-colon in favourites page

--- movie-app/src/pages/FavouritesPage.jsx
+++ movie-app/src/pages/FavouritesPage.jsx
@@ -11,22 +11,22 @@ export default function Favourites() {
   const favouritedMovies = { ...localStorage };
   const addedToFavouritesSvg = (
     <svg className="more-info" viewBox="0 0 800 800" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-    <g id="SVGRepo_iconCarrier">
-      <circle cx="400" cy="400" r="400" fill="#ffb525"></circle>
-      <path d="M200,400l141.42,141.42,282.81-282.84" stroke="#fff" stroke-width="66.67" stroke-linecap="round" stroke-linejoin="round" fill="none"></path>
-    </g>
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <circle cx="400" cy="400" r="400" fill="#ffb525"></circle>
+        <path d="M200,400l141.42,141.42,282.81-282.84" stroke="#fff" stroke-width="66.67" stroke-linecap="round" stroke-linejoin="round" fill="none"></path>
+      </g>
     </svg>)
 
   const addToFavouritesSvg = (
     <svg className="more-info" viewBox="0 0 800 800" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-    <g id="SVGRepo_iconCarrier">
-      <circle cx="400" cy="400" r="400" fill="#D3D3DD" opacity="0.5"></circle>
-      <path d="M200,400h200M400,400h200M400,400v200M400,400V200" stroke="#FFFFFF" stroke-width="66.6667" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="133.3333" fill="none"></path>
-    </g>
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <circle cx="400" cy="400" r="400" fill="#D3D3DD" opacity="0.5"></circle>
+        <path d="M200,400h200M400,400h200M400,400v200M400,400V200" stroke="#FFFFFF" stroke-width="66.6667" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="133.3333" fill="none"></path>
+      </g>
     </svg>)
 
   const moreInformationSvg = (
@@ -42,7 +42,7 @@ export default function Favourites() {
 
   return (
     <div className="main-body">
-      <h2 className={Object.keys(favouritedMovies).length === 0 ? "show-message" : "hide-message"}>You have no movies on your Favorites. Add your favorite movies later by clicking {addToFavouritesSvg} Add to Favorites.</h2>;
+      <h2 className={Object.keys(favouritedMovies).length === 0 ? "show-message" : "hide-message"}>You have no movies on your Favorites. Add your favorite movies later by clicking {addToFavouritesSvg} Add to Favorites.</h2>
       <div className="favourites-movie-container">
         {Object.keys(favouritedMovies).map((key) => {
           const stringifiedObject = localStorage.getItem(key);
