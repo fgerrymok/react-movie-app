5e14ade6d58a73952774dd99227cf3552b415644
Frazer <fgerrymok@gmail.com>
2024-08-26 17:05:11 -0700

    removed unused code

--- movie-app/vite.config.js
+++ movie-app/vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 export default defineConfig({
   base: '/click-flicks/',
-  // build: {
-  //   outDir: 'click-flicks'
-  // },
   plugins: [react()],
 })
